0c2fb3a49056afb28653ea04e9d24561ed14a6b7
dbudimir <dav.budimir@gmail.com>
2020-04-23 13:57:49 -0400

    push prod 3

--- init.sql
+++ init.sql
@@ -1,9 +1,3 @@
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS users_id_seq;
-
--- Table Definition
 CREATE TABLE "public"."users" (
     "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
     "user_full_name" varchar,
