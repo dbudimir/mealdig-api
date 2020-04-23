df9b22f220b49c3d288fb2b296fc3e547e14c3e3
dbudimir <dav.budimir@gmail.com>
2020-04-23 15:53:22 -0400

    push prod

--- config.js
+++ config.js
@@ -3,11 +3,21 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
+// const db = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.PORT
+// });
+
+// module.exports = db;
+
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const db = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+  //   ssl: isProduction
 })
 
 module.exports = { db }
